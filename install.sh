6b97b1330af774095620860c54eced256437a32b
Brian Pierce <brian.pierce@getcruise.com>
2017-08-31 15:57:52 -0700

    update script

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+cd ~/.emacs.d
+git checkout develop
+git pull --rebase origin develop
+cd -
 cp $DIR/spacemacs ~/.spacemacs
-cp -r $DIR/private/* ~/.emacs/private/
+cp -r $DIR/private/* ~/.emacs.d/private/
